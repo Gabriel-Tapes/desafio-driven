b4aed8d4fff1f49962046c58dcd03e8ed3cc9f50
Gabriel-Tapes <gabrielsouza.iacs@gmail.com>
2022-05-05 23:36:11 -0300

    exclusão de itens duplicados no repositório e adição do desafio 3

---
+++ to-do-list/script.js
@@ -0,0 +1,89 @@
+const prefixo = "card-";
+
+function adicionar(elemento) {
+    const texto = prompt("Qual é a tarefa?");
+
+    if (texto !== '' && texto !== null) {
+        const ulId = elemento.previousElementSibling.id;
+
+        const cardId = new Date().getTime()
+        const card = `
+        <li id="${cardId}" draggable="true" ondragstart="arrastar(event)">
+            <p class="tarefa" contenteditable="true" onblur="salvarAlteracoes(this)">
+                ${texto}
+            </p class="excluir">
+            <p class="excluir" onclick="deletar(this)">
+                x
+            </p>
+        </li>`;
+
+        const ul = document.getElementById(ulId);
+
+        ul.innerHTML = ul.innerHTML + card;
+        localStorage.setItem(prefixo + cardId, [card, ulId]);
+    }
+}
+
+function arrastar(event) {
+    event.dataTransfer.setData("tarefa", event.target.id);
+}
+
+function soltar(event, id) {
+    event.preventDefault();
+    const target = document.getElementById(id);
+    const data = event.dataTransfer.getData("tarefa");
+
+    const card = document.getElementById(data);
+
+    target.appendChild(card);
+
+    key = prefixo + data;
+    atual = localStorage[key].slice(localStorage[key].lastIndexOf(",") + 1);
+
+    localStorage[key] = localStorage[key].replace(atual, id);
+
+}
+
+function emCima(event) {
+    event.preventDefault();
+}
+
+function deletar(elemento) {
+    const idCard = elemento.parentElement.id;
+
+    card = document.getElementById(idCard);
+
+    card.remove();
+    localStorage.removeItem(prefixo + idCard);
+}
+
+function manterAlteracoes() {
+    for (let key in localStorage) {
+        if (key.includes(prefixo)) {
+            const ulId = localStorage[key].slice(localStorage[key].lastIndexOf(",") + 1);
+            const ul = document.getElementById(ulId);
+            const card = localStorage[key].slice(0, localStorage[key].lastIndexOf(","));
+
+            ul.innerHTML = ul.innerHTML + card;
+        }
+    }
+}
+
+function salvarAlteracoes(elemento) {
+    const card = elemento.parentElement;
+    const ulId = card.parentElement.id;
+    const cardId = card.id;
+    const texto = elemento.innerText;
+
+    localStorage[prefixo + cardId] = `
+    <li id="${cardId}" draggable="true">
+        <p class="tarefa" contenteditable="true" onblur="salvarAlteracoes(this)">
+            ${texto}
+        </p class="excluir">
+        <p class="excluir" onclick="deletar(this)">
+            x
+        </p>
+    </li>,${ulId}`;
+}
+
+manterAlteracoes();
